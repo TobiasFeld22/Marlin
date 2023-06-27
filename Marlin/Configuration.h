85350580cd5251cf876b55f20835caf012aa5efc
Tobias Feld <tobias.feld@hotmail.com>
2023-06-27 13:38:35 +0200

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1616,7 +1616,7 @@
 #define PROBING_MARGIN 20
 
 // X and Y axis travel speed (mm/min) between probes
-#define XY_PROBE_FEEDRATE (50 * 60)
+#define XY_PROBE_FEEDRATE (100 * 60)
 
 // Feedrate (mm/min) for the first approach when double-probing (MULTIPLE_PROBING == 2)
 #define Z_PROBE_FEEDRATE_FAST (4 * 60)
